d34813e4e53e4d6a764a49b06889cb281923844f
Navya Manoj P <navyamanojp123@gmail.com>
2026-09-01 11:39:18 +0530

    backend

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test route
+app.get("/", (req, res) => {
+    res.json({
+        message: "HackMate Backend is running!"
+    });
+});
+
+// Port
+const PORT = process.env.PORT || 5000;
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
