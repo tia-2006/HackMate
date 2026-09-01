aa4b19955962cf454d66ef0a03ad1df6b57428da
Navya Manoj P <navyamanojp123@gmail.com>
2026-09-01 11:52:05 +0530

    mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 
+const connectDB = require("./config/db");
+
 const app = express();
+
+// Connect to MongoDB
+connectDB();
 
 // Middleware
 app.use(cors());
@@ -18,7 +23,6 @@ app.get("/", (req, res) => {
 // Port
 const PORT = process.env.PORT || 5000;
 
-// Start server
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
